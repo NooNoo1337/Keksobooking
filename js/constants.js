aaa2ceb5eb9d8b8b3210ea1b6ccea131855c7e9e
Mikhail Rerberg <misha1247@mail.ru>
2017-12-27 18:27:12 +0300

    Правки

--- js/constants.js
+++ js/constants.js
@@ -7,7 +7,8 @@
     similarPinElement: document.querySelector('.map__pins'),
     PIN_COORDINATES: {},
     MIN_PIN_COORDINATE: 125,
-    MAX_PIN_COORDINATE: 650
+    MAX_PIN_COORDINATE: 650,
+    mainForm: document.querySelector('.notice__form')
   };
 
 })();
--- js/map.js
+++ js/map.js
@@ -3,7 +3,6 @@
 // variables
 var mapBlock = document.querySelector('.map');
 var mainPin = document.querySelector('.map__pin--main');
-var mainForm = document.querySelector('.notice__form');
 var allFieldsets = document.querySelectorAll('fieldset');
 var ESC_BUTTON = 27;
 var ENTER_BUTTON = 13;
@@ -18,27 +17,10 @@ var renderAllPins = function () {
 };
 
 
-// render all map announcement
-var createPopup = function (number) {
-  window.constants.fragment.appendChild(window.card.createAnnouncement(window.data.announcementsCollection[number]));
-  mapBlock.appendChild(window.constants.fragment);
-  document.querySelector('.popup').classList.remove('hidden');
-
-  // создание события закрытия окна информации по клику и по нажатию на Enter
-  var closePopupButton = mapBlock.querySelector('.popup__close');
-  closePopupButton.addEventListener('click', closeCurrentAnnouncement);
-  closePopupButton.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ENTER_BUTTON) {
-      closeCurrentAnnouncement(evt);
-    }
-  });
-};
-
-
 // activate map
 var activateMap = function () {
   mapBlock.classList.remove('map--faded');
-  mainForm.classList.remove('notice__form--disabled');
+  window.constants.mainForm.classList.remove('notice__form--disabled');
 
   allFieldsets.forEach(function (item) {
     item.removeAttribute('disabled');
@@ -68,66 +50,16 @@ mainPin.addEventListener('keydown', function (evt) {
 });
 
 
-var checkEscButton = function (evt) {
-  if (evt.keyCode === ESC_BUTTON) {
-    closeCurrentAnnouncement(evt);
-  }
-};
-
-var deactivatePin = function () {
-  var selectedPin = document.querySelector('.map__pin--active');
-
-  if (selectedPin) {
-    selectedPin.classList.remove('map__pin--active');
-  }
-};
-
-
-var closePopup = function () {
-  var mapCard = document.querySelector('.map__card');
-
-  if (mapCard) {
-    mapBlock.removeChild(mapCard);
-  }
-};
-
-
-var closeCurrentAnnouncement = function (evt) {
-  closePopup();
-  deactivatePin();
-  document.removeEventListener('keydown', checkEscButton);
-  evt.stopPropagation();
-};
-
-// show popup
-var showPopup = function (evt) {
-  var target = evt.target;
-  document.addEventListener('keydown', checkEscButton);
-  while (target !== mapBlock) {
-    if (target.className === 'map__pin') {
-      closePopup();
-      deactivatePin();
-      target.classList.add('map__pin--active');
-      var pinId;
-      pinId = target.id.replace('pin-', '');
-      createPopup(pinId, evt);
-      return;
-    }
-    target = target.parentNode;
-  }
-};
-
-mapBlock.addEventListener('click', showPopup);
+mapBlock.addEventListener('click', window.showPopup);
 
 mapBlock.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_BUTTON) {
-    showPopup(evt);
+    window.showPopup(evt);
   }
 });
 
 var dragPinMain = function (evt) {
   evt.preventDefault();
-
 
   var startCoordinates = {
     x: evt.clientX,
