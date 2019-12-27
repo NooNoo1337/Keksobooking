61846f537b7ca1387b8b94fd7e743c7b439bd22b
Ashh <HeyAshh@users.noreply.github.com>
2019-12-27 18:59:54 +0300

    Михаил бог

--- js/map.js
+++ js/map.js
@@ -1,24 +1,23 @@
-{
-  "author": {
-    "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где xx это число от 1 до 8 с ведущим нулем. Например 01, 02 и т. д. Адреса изображений не повторяются
-  },
+'use strict';
 
-  "offer": {
-    "title": строка, заголовок предложения, одно из фиксированных значений "Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде". Значения не должны повторяться.
-    "address": строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}"
-    "price": число, случайная цена от 1000 до 1 000 000
-    "type": строка с одним из трех фиксированных значений: flat, house или bungalo
-    "rooms": число, случайное количество комнат от 1 до 5
-    "guests": число, случайное количество гостей, которое можно разместить
-    "checkin": строка с одним из трех фиксированных значений: 12:00, 13:00 или 14:00,
-    "checkout": строка с одним из трех фиксированных значений: 12:00, 13:00 или 14:00
-    "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-    "description": пустая строка,
-    "photos": пустой массив
-  },
+const COUNT_ADVERTISMENT = 8
 
-  "location": {
-    "x": случайное число, координата x метки на карте от 300 до 900,
-    "y": случайное число, координата y метки на карте от 100 до 500
-  }
+function getAvatar(avatarNumber = 0) {
+    return `../img/avatars/user0${avatarNumber}.png`;
+    
 }
+
+function generateAdvertisment(advertismentCount) {
+  const advertismentsColletion = [];
+
+  for(let i = 1; i <= advertismentCount; i++) {
+    advertismentsColletion.push({
+        avatar: getAvatar(i),
+
+    })
+  }
+
+  return advertismentsColletion;
+}
+
+console.log(generateAdvertisment(COUNT_ADVERTISMENT));
